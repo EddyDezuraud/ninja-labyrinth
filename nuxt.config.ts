463575ca4c723972df999fb41293d60f171381c4
Eddy Dezuraud <eddy@gedivote.fr>
2025-01-18 16:05:39 +0100

    feat: Add variables.css for custom styling

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: ['@nuxtjs/storybook'],
+  css: [
+    '~/assets/styles/variables.css', // Ou variables.scss si tu utilises SCSS
+  ],
 })
